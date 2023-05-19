2d93770dbcb060be4e427cc5e52dca8c32a94885
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-05-18 20:21:25 -0500

    Add files via upload

---
+++ src/EDIT/mod.rs
@@ -0,0 +1,19 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::lua_const::*,
+		hash40
+    },
+    smash_script::*,
+    smashline::*
+};
+
+
+
+pub fn install() {
+    smashline::install_acmd_scripts!(
+        
+    );
+}
