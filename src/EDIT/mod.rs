231e00c374d83b16a51a34a0b48438c2771e3486
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-12-11 19:44:39 -0600

    Added code examples, added usage to readme

--- src/EDIT/mod.rs
+++ src/EDIT/mod.rs
@@ -3,17 +3,52 @@ use {
         lua2cpp::*,
         phx::*,
         app::{sv_animcmd::*, lua_bind::*, *},
-        lib::lua_const::*,
+        lib::{lua_const::*, L2CValue, L2CAgent},
 		hash40
     },
     smash_script::*,
-    smashline::*
+	smashline::*
 };
 
+// Game acmd script
+#[acmd_script( agent = "AGENTNAME", script = "GAME_NAME", category = ACMD_GAME, low_priority )]
+unsafe fn ACMDSCRIPTNAME(agent: &mut L2CAgentBase) {
 
+}
+
+// Global opff
+#[fighter_frame_callback]
+pub fn global_fighter_frame(fighter : &mut L2CFighterCommon) {
+    unsafe {
+
+    }
+}
+
+// Char opff
+#[fighter_frame( agent = FIGHTER_KIND_ )]
+fn AGENTFRAMENAME(fighter: &mut L2CFighterCommon) {
+    unsafe {
+
+	}
+}
+
+// Status Script
+#[status_script(agent = "AGENT", status = STATUS, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
+unsafe extern "C" fn STATUSSCRIPTNAME(fighter: &mut L2CFighterCommon) -> L2CValue {
+
+}
 
 pub fn install() {
     smashline::install_acmd_scripts!(
-        
+        ACMDSCRIPTNAME
     );
+    smashline::install_agent_frame_callbacks!(
+		global_fighter_frame
+    );
+    smashline::install_agent_frames!(
+        AGENTFRAMENAME
+    );
+    install_status_scripts!(
+		STATUSSCRIPTNAME
+	);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,12 @@
 )]
 #![allow(
     unused_imports,
-    unused_parens,
 	unused_macros,
 	unused_variables,
 	unused_assignments,
+	unused_unsafe,
 	non_upper_case_globals,
 	non_snake_case,
-	dead_code,
     clippy::borrow_interior_mutable_const
 )]
 
