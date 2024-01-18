7aa25eee46b265f5d44b135202468f05cfc53674
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-01-18 14:40:12 -0600

    Now builds without edit

--- src/EDIT/mod.rs
+++ src/EDIT/mod.rs
@@ -4,55 +4,55 @@ use {
         phx::*,
         app::{sv_animcmd::*, lua_bind::*, *},
         lib::{lua_const::*, L2CValue, L2CAgent},
-		hash40
+        hash40
     },
     smash_script::*,
-	smashline::*
+    smashline::*
 };
 
 // Game acmd script
-#[acmd_script( agent = "AGENTNAME", script = "GAME_NAME", category = ACMD_GAME, low_priority )]
-unsafe fn ACMDSCRIPTNAME(agent: &mut L2CAgentBase) {
-
+#[acmd_script( agent = "mario", script = "game_ATTACK_NAME_HERE", category = ACMD_GAME, low_priority )]
+unsafe fn example_acmd_script(agent: &mut L2CAgentBase) {
+    
 }
 
 // Global opff
 #[fighter_frame_callback]
 pub fn global_fighter_frame(fighter : &mut L2CFighterCommon) {
     unsafe {
-
+        
     }
 }
 
 // Char opff
-#[fighter_frame( agent = FIGHTER_KIND_ )]
-fn AGENTFRAMENAME(fighter: &mut L2CFighterCommon) {
+#[fighter_frame( agent = FIGHTER_KIND_MARIO )]
+fn mario_frame(fighter: &mut L2CFighterCommon) {
     unsafe {
-
-	}
+        
+    }
 }
 
 // Status Script
-#[status_script(agent = "AGENT", status = STATUS, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
-unsafe extern "C" fn STATUSSCRIPTNAME(fighter: &mut L2CFighterCommon) -> L2CValue {
-
+#[status_script(agent = "mario", status = FIGHTER_MARIO_STATUS_KIND_SPECIAL_LW_CHARGE, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
+unsafe extern "C" fn example_status_script(fighter: &mut L2CFighterCommon) -> L2CValue {
+    0.into()
 }
 
 pub fn install() {
-	// Game, effect, sound, expression
+    // Game, effect, sound, expression
     smashline::install_acmd_scripts!(
-        ACMDSCRIPTNAME
+        example_acmd_script
     );
-	// Global fighter frame
+    // Global fighter frame
     smashline::install_agent_frame_callbacks!(
-		global_fighter_frame
+        global_fighter_frame
     );
-	// Agent fighter frame
+    // Agent fighter frame
     smashline::install_agent_frames!(
-        AGENTFRAMENAME
+        mario_frame
     );
-	// Status Script
+    // Status Script
     install_status_scripts!(
-		STATUSSCRIPTNAME
-	);
+        example_status_script
+    );
 }
