ac65f3fcd6492c6214ef36cbf741c76ba32ef1e0
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-02-26 16:10:36 -0600

    Added global opff

--- src/EDIT/mod.rs
+++ src/EDIT/mod.rs
@@ -15,22 +15,25 @@ unsafe extern "C" fn example_acmd_script(agent: &mut L2CAgentBase) {
     
 }
 
-// Char opff
+// Char opff, Global opff
 unsafe extern "C" fn fighter_frame(fighter: &mut L2CFighterCommon) {
     unsafe {
         
     }
 }
 
-// Status Script
+// Status script
 unsafe extern "C" fn example_status_script(fighter: &mut L2CFighterCommon) -> L2CValue {
     0.into()
 }
 
 pub fn install() {
     Agent::new("mario")
-        .game_acmd("game_ATTACK_NAME_HERE", example_acmd_script)
-        .on_line(Main, fighter_frame)
-        .status(Main, *FIGHTER_MARIO_STATUS_KIND_SPECIAL_LW_CHARGE, example_status_script)
+        .game_acmd("game_ATTACK_NAME_HERE", example_acmd_script) // Game acmd script
+        .on_line(Main, fighter_frame) // Char opff
+        .status(Main, *FIGHTER_MARIO_STATUS_KIND_SPECIAL_LW_CHARGE, example_status_script) // Status script
+        .install();
+    Agent::new("fighter")
+        .on_line(Main, fighter_frame) // Global opff
         .install();
 }
